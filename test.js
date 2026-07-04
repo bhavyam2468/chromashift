3f5a7717311f8ecc2a30026ee0c86ee8c8f0aa3b
bhavyam2468 <bhavyam2468@users.noreply.github.com>
2026-07-05 00:03:18 +0530

    chore: clean up temporary test scripts from git

--- test.js
+++
@@ -1,9 +0,0 @@
-import puppeteer from 'puppeteer';
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  page.on('console', msg => console.log('BROWSER_CONSOLE:', msg.text()));
-  page.on('pageerror', error => console.log('BROWSER_ERROR:', error.message));
-  await page.goto('http://localhost:5175/', { waitUntil: 'networkidle0' });
-  await browser.close();
-})();
